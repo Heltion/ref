0d7ee3d5aa335c46b114d030f532ab821ea6ff5f
Heltion <heltion@qq.com>
2023-09-14 22:57:14 +0800

    M  main.tex A  string/lyndon/main.cpp A  string/lyndon/yosupo.cpp

---
+++ string/lyndon/main.cpp
@@ -0,0 +1,9 @@
+vector<int> lyndon_factorization(string const &s) {
+  vector<int> res = {0};
+  for (int i = 0, n = s.size(); i < n;) {
+    int j = i + 1, k = i;
+    for (; j < n and s[k] <= s[j]; j += 1) { k = s[k] < s[j] ? i : k + 1; }
+    while (i <= k) { res.push_back(i += j - k); }
+  }
+  return res;
+}
---
+++ string/lyndon/yosupo.cpp
@@ -0,0 +1,21 @@
+// https://judge.yosupo.jp/problem/runenumerate
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> lyndon_factorization(string const &s) {
+  vector<int> res = {0};
+  for (int i = 0, n = s.size(); i < n;) {
+    int j = i + 1, k = i;
+    for (; j < n and s[k] <= s[j]; j += 1) { k = s[k] < s[j] ? i : k + 1; }
+    while (i <= k) { res.push_back(i += j - k); }
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  for (int ai : lyndon_factorization(s)) { cout << ai << " "; }
+}
