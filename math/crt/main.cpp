3db08dd14ca0910b6d7e70c6597fd07fa9255974
yaoweilyu <lvyaowei@ft.tech>
2023-11-17 15:52:52 +0800

    M  .DS_Store M  graph/mcmf/main.cpp M  main.pdf M  math/crt/main.cpp

--- math/crt/main.cpp
+++ math/crt/main.cpp
@@ -3,7 +3,7 @@ tuple<i64, i64, i64> exgcd(i64 a, i64 b) {
   while (b) {
     i64 q = a / b;
     tie(x, x1) = pair(x1, x - q * x1);
-    tie(y, y1) = pair(y1, x - q * y1);
+    tie(y, y1) = pair(y1, y - q * y1);
     tie(a, b) = pair(b, a - q * b);
   }
   return {a, x, y};
